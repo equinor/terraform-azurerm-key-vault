5e7a9390b6e71755edf86fd3d6b3333b902ef234
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-03-08 09:40:12 +0100

    Update provider features for example

--- examples/key-vault-with-secret/main.tf
+++ examples/key-vault-with-secret/main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+      purge_soft_delete_on_destroy    = false
+    }
+  }
 }
 
 locals {

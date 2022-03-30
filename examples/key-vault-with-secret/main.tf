be429d372277bc27acd6a00367b8c24a13af8717
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-03-30 09:11:16 +0200

    chore: update examples
    Update examples to create resources with unique names to prevent issues if a soft-deleted key vault exists. Also update examples to purge key vault when destroying.

--- examples/key-vault-with-secret/main.tf
+++ examples/key-vault-with-secret/main.tf
@@ -1,26 +1,26 @@
 provider "azurerm" {
-  features {
-    key_vault {
-      recover_soft_deleted_key_vaults = true
-      purge_soft_delete_on_destroy    = false
-    }
-  }
+  features {}
 }
 
 locals {
   app_name         = "ops-vault"
-  environment_name = "example"
+  environment_name = random_integer.this.result
 }
 
-resource "azurerm_resource_group" "example" {
+resource "random_integer" "this" {
+  min = 0
+  max = 32767
+}
+
+resource "azurerm_resource_group" "this" {
   name     = "rg-${local.app_name}-${local.environment_name}"
   location = "northeurope"
 }
 
-resource "azurerm_log_analytics_workspace" "example" {
+resource "azurerm_log_analytics_workspace" "this" {
   name                = "log-${local.app_name}-${local.environment_name}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
   sku                 = "Free"
 }
 
@@ -29,12 +29,18 @@ module "vault" {
 
   app_name                   = local.app_name
   environment_name           = local.environment_name
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+  location                   = azurerm_resource_group.this.location
+  resource_group_name        = azurerm_resource_group.this.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+
+  client_permissions = {
+    certificates = []
+    keys         = []
+    secrets      = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
+  }
 }
 
-resource "azurerm_key_vault_secret" "example" {
+resource "azurerm_key_vault_secret" "this" {
   name         = "secret-name"
   value        = "super-secret-value"
   key_vault_id = module.vault.key_vault_id

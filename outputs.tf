32553b3947c488d5e33ad9804153e5edb0d1f8f6
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-08-16 15:47:03 +0200

    feat!: remove unnecessary output

--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,3 @@ output "vault_uri" {
   description = "The URI of the Key Vault."
   value       = azurerm_key_vault.this.vault_uri
 }
-
-output "monitor_diagnostic_setting_id" {
-  description = "The ID of the Monitor Diagnostic Setting."
-  value       = azurerm_monitor_diagnostic_setting.this.id
-}

9cbc71ca724b46b8ea7471a11074c932d642ecf5
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-08-16 15:52:29 +0200

    chore: update test and fixture

--- test/terraform_vault_test.go
+++ test/terraform_vault_test.go
@@ -1,43 +1,14 @@
 package test
 
 import (
-	"io"
-	"net/http"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-const TerraformDir = "./fixture"
-
 func TestTerraformVault(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: TerraformDir,
-	})
-
-	defer terraform.Destroy(t, terraformOptions)
-
-	terraform.InitAndApply(t, terraformOptions)
-}
-
-func TestTerraformVaultFirewall(t *testing.T) {
-	// Get public IP address
-	resp, err := http.Get("https://ifconfig.me/")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	ip, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: TerraformDir,
-
-		Vars: map[string]interface{}{
-			"firewall_ip_rules": []string{string(ip)},
-		},
+		TerraformDir: "./fixture",
 	})
 
 	defer terraform.Destroy(t, terraformOptions)

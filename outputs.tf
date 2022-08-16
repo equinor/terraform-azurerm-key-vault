489f4d86d4c6a0410328cb7be08cd60fa2d0e8f8
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-08-16 15:46:35 +0200

    feat!: remove unnecessary output

--- outputs.tf
+++ outputs.tf
@@ -8,11 +8,6 @@ output "vault_id" {
   ]
 }
 
-output "vault_name" {
-  description = "The name of the Key Vault."
-  value       = azurerm_key_vault.this.name
-}
-
 output "vault_uri" {
   description = "The URI of the Key Vault."
   value       = azurerm_key_vault.this.vault_uri

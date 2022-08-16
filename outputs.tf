9cdb63c6f6eb8b9632ea01cef20832ee64f6b673
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-08-16 15:46:17 +0200

    feat!: rename outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "key_vault_id" {
+output "vault_id" {
   description = "The ID of the Key Vault."
   value       = azurerm_key_vault.this.id
 
@@ -8,12 +8,12 @@ output "key_vault_id" {
   ]
 }
 
-output "key_vault_name" {
+output "vault_name" {
   description = "The name of the Key Vault."
   value       = azurerm_key_vault.this.name
 }
 
-output "key_vault_uri" {
+output "vault_uri" {
   description = "The URI of the Key Vault."
   value       = azurerm_key_vault.this.vault_uri
 }

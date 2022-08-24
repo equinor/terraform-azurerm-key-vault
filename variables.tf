a3bf50c06bfffd494b0a248280e0e65ffa72d281
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-08-24 17:38:02 +0200

    chore: update variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,32 +1,26 @@
 variable "vault_name" {
-  description = "The name of this Key Vault."
-  type        = string
-}
-
-variable "location" {
-  description = "The supported Azure location where the resources exist."
+  description = "The name of this Key vault."
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the resources."
+  description = "The name of the resource group to create the resources in."
   type        = string
 }
 
-variable "tags" {
-  description = "A mapping of tags to assign to the resources."
-  type        = map(string)
-  default     = {}
+variable "location" {
+  description = "The location to create the resources in."
+  type        = string
 }
 
 variable "purge_protection_enabled" {
-  description = "Is purge protection enabled for this key vault?"
+  description = "Is purge protection enabled for this Key vault?"
   type        = bool
   default     = false
 }
 
 variable "access_policies" {
-  description = "A list of access policies for this Key Vault."
+  description = "A list of access policies for this Key vault."
   type = list(object({
     object_id               = string
     secret_permissions      = list(string)
@@ -37,18 +31,24 @@ variable "access_policies" {
 }
 
 variable "firewall_ip_rules" {
-  description = "A list of IP addresses or CIDR blocks that should be able to access the Key Vault."
+  description = "A list of IP addresses or CIDR blocks that should be able to access this Key vault."
   type        = list(string)
   default     = []
 }
 
 variable "firewall_subnet_rules" {
-  description = "A list of IDs of the subnets that should be able to access the Key Vault."
+  description = "A list of IDs of the subnets that should be able to access this Key vault."
   type        = list(string)
   default     = []
 }
 
 variable "log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics Workspace to send diagnostics to."
+  description = "The ID of the Log Analytics workspace to send diagnostics to."
   type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
 }

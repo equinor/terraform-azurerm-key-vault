02dc86aca647199ec605d8312de5accbcfae9dc6
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-09-23 13:31:01 +0200

    fix: set empty app id

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ locals {
   access_policies = [
     for p in var.access_policies : {
       tenant_id               = data.azurerm_client_config.current.tenant_id
+      application_id          = ""
       object_id               = p.object_id
       secret_permissions      = p.secret_permissions
       certificate_permissions = p.certificate_permissions

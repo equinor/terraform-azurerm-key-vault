205a68cdd5b0a3511196003a5388774f8a38c92c
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-12-22 15:39:42 +0100

    refactor: update variable descriptions

--- variables.tf
+++ variables.tf
@@ -31,19 +31,19 @@ variable "access_policies" {
 }
 
 variable "network_acls_ip_rules" {
-  description = "A list of IP addresses or CIDR blocks that should be able to access this Key vault."
+  description = "A list of IP addresses or CIDR blocks that should be able to bypass the network ACL and access this Key vault."
   type        = list(string)
   default     = []
 }
 
 variable "network_acls_virtual_network_subnet_ids" {
-  description = "A list of IDs of the subnets that should be able to access this Key vault."
+  description = "A list of Virtual Network subnet IDs that should be able to bypass the network ACL and access this Key vault."
   type        = list(string)
   default     = []
 }
 
 variable "network_acls_bypass_azure_services" {
-  description = "Should Azure services be able to bypass the Key vault firewall?"
+  description = "Should Azure services be able to bypass the network ACL and access this Key vault?"
   type        = bool
   default     = true
 }

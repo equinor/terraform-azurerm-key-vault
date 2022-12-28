3c1c5649114f820198684ae3e648cf6327bbc605
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-12-28 10:16:34 +0100

    feat: set custom diagnostic setting name

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_key_vault" "this" {
 }
 
 resource "azurerm_monitor_diagnostic_setting" "this" {
-  name                       = "${azurerm_key_vault.this.name}-logs"
+  name                       = var.diagnostic_setting_name
   target_resource_id         = azurerm_key_vault.this.id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 

adac68912d3b8bf49e1902cdc000bfff6cc33a57
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-10-25 12:37:52 +0200

    fix: update basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -21,7 +21,6 @@ module "log_analytics" {
 resource "azurerm_monitor_action_group" "this" {
   name                = "ag-${random_id.this.hex}"
   resource_group_name = var.resource_group_name
-  location            = var.location
   short_name          = "Action"
 }
 

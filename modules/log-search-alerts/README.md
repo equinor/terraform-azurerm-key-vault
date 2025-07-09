868ac73573d77a3eb80c1bb266c3c1c8e3fd1341
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2025-07-09 10:56:09 +0200

    refactor: rename module

---
+++ modules/log-search-alerts/README.md
@@ -0,0 +1,3 @@
+# Log search alerts submodule
+
+Terraform submodule which creates log search alert rules for Azure Key Vault.
--- modules/log-search-alerts/main.tf
+++ modules/log-search-alerts/main.tf
@@ -1,42 +1,39 @@
-# Ref: https://learn.microsoft.com/en-us/azure/key-vault/general/alert#example-log-query-alert-for-near-expiry-certificates (2025-07-08)
-
 resource "azurerm_monitor_scheduled_query_rules_alert_v2" "secret_near_expiry" {
   name                = var.secret_near_expiry_alert_rule_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  scopes              = var.vault_ids
+  scopes              = var.scopes
 
   criteria {
     query = <<-QUERY
       AzureDiagnostics
         | where OperationName == "SecretNearExpiryEventGridNotification"
-        | extend SecretExpire = unixtime_seconds_todatetime(eventGridEventProperties_data_EXP_d)
-        | extend DaysTillExpire = datetime_diff("Day", SecretExpire, now())
-        | project ResourceId, SecretName = eventGridEventProperties_subject_s, DaysTillExpire, SecretExpire
+        | extend SecretExpiryDatetime = unixtime_seconds_todatetime(eventGridEventProperties_data_EXP_d)
+        | extend SecretExpiryDays = datetime_diff("Day", SecretExpiryDatetime, now())
+        | project ResourceId, SecretName = eventGridEventProperties_subject_s, SecretExpiryDatetime, SecretExpiryDays
     QUERY
 
-    time_aggregation_method = "Count" # TODO: aggregation granularity?
+    time_aggregation_method = "Count"
     resource_id_column      = "ResourceId"
     operator                = "GreaterThan"
     threshold               = 0
 
-    # TODO: Ensure unique SecretName + DaysTillExpire combinations will trigger separate alerts
+    # TODO: Ensure unique SecretName + SecretExpiryDays combinations will trigger separate alerts
     dimension {
       name     = "SecretName"
       operator = "Include"
       values   = ["*"]
     }
-
     dimension {
-      name     = "DaysTillExpire"
+      name     = "SecretExpiryDays"
       operator = "Include"
       values   = ["*"]
     }
   }
 
   # Configure alert to run once per day, and evaluate logs from the entire previous day
-  evaluation_frequency = "P1D" # TODO
-  window_duration      = "P1D" # TODO
+  evaluation_frequency = "P1D"
+  window_duration      = "P1D"
 
   severity = 2 # Warning
 

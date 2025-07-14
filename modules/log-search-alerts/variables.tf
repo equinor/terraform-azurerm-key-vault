5abba4f9e0a65bd5ab6d4faa61cebbd93da9ae56
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2025-07-14 10:36:04 +0200

    feat: create secret near expiry alerts (#113)
    * wip: create object neary expiry alerts
    
    * feat: create `alerts` submodule
    
    * CRLF -> LF
    
    * refactor: rename module
    
    * docs: clarify comments
    
    * refactor: correct query indents
    
    * docs: consistent comment punctuation
    
    * chore: set required Terraform and provider versions
    
    * docs: update README

---
+++ modules/log-search-alerts/variables.tf
@@ -0,0 +1,31 @@
+variable "resource_group_name" {
+  description = "The name of the resource group to create the resources in."
+  type        = string
+}
+
+variable "location" {
+  description = "The location to create the resources in."
+  type        = string
+}
+
+variable "scopes" {
+  description = "A list of scopes (resource IDs) to run the log queries at."
+  type        = list(string)
+}
+
+variable "action_group_ids" {
+  description = "A list of action group IDs to send alerts to."
+  type        = list(string)
+}
+
+variable "secret_near_expiry_alert_rule_name" {
+  description = "The name of this Secret Near Expiry alert rule."
+  type        = string
+  default     = "Secret Near Expiry Alert"
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
+}
